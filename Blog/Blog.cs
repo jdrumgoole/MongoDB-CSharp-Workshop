1aac7d9dedc2e6302f53545c6fe863bba657a8dc
Joe Drumgoole <Joe.Drumgoole@mongodb.com>
2019-07-07 15:37:16 +0100

    Removed junk adde blog

---
+++ Blog/Blog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Blog
+{
+    class Blog
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient(); // mongodb://localhost:27017 is the default
+            var database = client.GetDatabase("Blog");
+            var users = database.GetCollection<BsonDocument>("users");
+
+            var user_joe = new BsonDocument
+            {
+                {"username", "jdrumgoole"},
+                {"password", "a secret"},
+                {
+                    "locale", new BsonDocument
+                    {
+                        {"lang", "EN"},
+                        {"currency", "sterling"},
+                    }
+                },
+            };
+
+            users.InsertOne(user_joe);
+            Console.WriteLine("Added user_joe");
+
+            var user_jim = new BsonDocument
+            {
+                {"username", "jim"},
+                {"password", "a different secret"},
+            };
+
+            var locale = new BsonDocument();
+            locale.Add("lang", new BsonString("DE"));
+            locale.Add("currency", new BsonString("euro"));
+            user_jim.Add("locale", locale);
+
+            users.InsertOne(user_jim);
+            Console.WriteLine("Added user_jim");
+            Console.ReadKey();
+        }
+    }
+}
--- Scratch/Scratch.cs
+++ Scratch/Scratch.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Xsl;
 using MongoDB.Driver;
 using MongoDB.Bson;
 
@@ -13,12 +15,41 @@ namespace Scratch
         static void Main(string[] args)
         {
 
-            foreach (var x in Enumerable.Range(0, 10)) //.Select(i=> new BsonDocument("counter", i )))
-            {
-                Console.WriteLine(x);
+            //foreach (var x in Enumerable.Range(0, 10)) //.Select(i=> new BsonDocument("counter", i )))
+            //{
+            //    Console.WriteLine(x);
 
+            //}
+
+            var client = new MongoClient();
+            var db = client.GetDatabase("scratch");
+            var col = db.GetCollection<BsonDocument>("scratch");
+
+            var dict = new Dictionary<string, string>() { { "a", "b" }, { "1", "2" } };
+            foreach (var item in dict)
+            {
+                Console.WriteLine("Insert {0}:{1}", item.Key, item.Value);
+                col.InsertOne(new BsonDocument(new BsonElement(item.Key, item.Value)));
             }
 
+            var doc = new BsonDocument {{"a", "b"}, {"1", "2"}};
+            Console.WriteLine(doc);
+            var docElement = new BsonElement("key", "value");
+            Console.WriteLine(docElement);
+
+            var intElement = new BsonElement("age", 30);
+            Console.WriteLine(intElement);
+
+            var dateTimeElement = new BsonElement("timestamp", DateTime.UtcNow);
+            Console.WriteLine(dateTimeElement);
+
+            IFindFluent<BsonDocument, BsonDocument > cursor =  col.Find(new BsonDocument());
+
+            Console.WriteLine("All docs");
+            foreach (var i in cursor.ToEnumerable())
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
     }
